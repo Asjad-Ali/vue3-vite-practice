7f50cce6803dbe600351b8cd293189a966015d49
Asjad Ali <asjad.ali@manafatech.com>
2024-04-04 17:16:05 +0500

    font awesom

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from './router'
-import "bootstrap/dist/js/bootstrap.js"
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/js/bootstrap.js"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
+library.add(fas);
 const app = createApp(App);
 
 app
   .use(router)
 
-  .mount("#app");
+  .component('fa', FontAwesomeIcon) // Register the Vue component globally
+  .mount('#app');
