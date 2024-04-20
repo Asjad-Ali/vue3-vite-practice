a782aef1573840ffb6417ec1285db34e20fbf44b
Asjad Ali <asjad.ali@manafatech.com>
2024-04-20 19:29:32 +0500

    router gaurd

--- src/router/index.js
+++ src/router/index.js
@@ -18,4 +18,16 @@ const routes = [
 
 const router = createRouter({ history: createWebHistory(), routes })
 
+// router.beforeEach((to, from, next) => {
+//   const isAuthenticated = localStorage.getItem('user') ? true : false
+//   if (to.name == 'home' && isAuthenticated) {
+//     next({ path: '/dashboard' });
+//   }
+//   else if (to.name !== 'home' && !isAuthenticated) {
+//     next({ path: '/' });
+//   } else {
+//     next()
+//   }
+// })
+
 export default router
