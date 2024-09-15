31406caaa75ec3fec2a72c658aa8a008e66f2874
Asjad Ali <asjadaliwatto@gmail.com>
2024-09-15 12:30:00 +0500

    update 26

--- src/Pages/App.vue
+++ src/Pages/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>This is app page name update test for commit yesterday and 26</h1>
+    <h1>This is app page name update test for commit yesterday and 15</h1>
   </div>
 </template>
