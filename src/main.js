486e664ef6b9bed105a17940e90e041247281165
Asjad Ali <asjadaliwatto@gmail.com>
2024-09-15 17:42:03 +0500

    pagination update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router'
 import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
