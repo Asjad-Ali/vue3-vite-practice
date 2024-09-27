62efa365b4da26952ce4bb0c21f135709ef4b665
Asjad Ali <asjadaliwatto@gmail.com>
2024-09-27 22:15:24 +0500

    delete router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+import VuePdfEmbed, { useVuePdfEmbed } from "vue-pdf-embed";
+
+const { doc } = useVuePdfEmbed({
+  source:
+    "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf",
+});
+</script>
+
 <template>
-  <router-view></router-view>
+  <div>
+    <VuePdfEmbed :source="doc" />
+  </div>
 </template>
